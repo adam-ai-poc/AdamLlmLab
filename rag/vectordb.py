fbe3b079994770b5519e7b40c77bb31a1e83132f
adamtay <tayxhwork@gmail.com>
2024-06-30 22:19:28 +0800

    Refactor: vectordb from_document to use kwargs. Enhance: enable persisting db to disk.

--- rag/vectordb.py
+++ rag/vectordb.py
@@ -15,21 +15,24 @@ EMBEDDING = {
 Base Vector Database class 
 '''
 class VectorDB:
-    def __init__(self, vectorstore, embedding_model, debug):
+    def __init__(self, vectorstore, embedding_model, vectordb_cfg:dict={}, debug=False):
         self.vectorstore=vectorstore
         self.vectorstore_name = self.vectorstore.__class__.__name__
         self.embedding_model = embedding_model
+        self.vectordb_cfg = vectordb_cfg
         self.debug = debug
         if self.debug:
             print("==========================================")
             print("Vector database configurations")
             print("==========================================")
             print("Vector Database:", self.vectorstore_name)
-            print("Embedding model:", embedding_model.model)
+            print("Embedding model:", self.embedding_model.model)
+            print("Vector Database configs: ", self.vectordb_cfg)
             print("==========================================")
 
     def store(self, chunks):
-        self.vectorstore = self.vectorstore.from_documents(chunks, self.embedding_model)
+        self.vectordb_cfg["documents"] = chunks
+        self.vectorstore = self.vectorstore.from_documents(**self.vectordb_cfg)
 
     def as_retriever(self, search_type, search_kwargs):
         self.search_type = search_type
@@ -47,15 +50,19 @@ class ChromaDB(VectorDB):
     def __init__(self, vectordb_cfg: dict=vectordb_cfg, debug=False):
         # VectorDB
         self.vectorstore = Chroma()
+        self.vectordb_cfg = vectordb_cfg["chroma"]
         self.num_chunks = 0
 
-        # Embedding
+        # Parse embedding config
         self.embedding_backend = next(iter(self.vectordb_cfg.get("embedding"))) 
-        self.embedding_model_name = self.vectordb_cfg["embedding"][self.embedding_backend]["model_name"]
-        self.embedding_model = EMBEDDING[self.embedding_backend](model=self.embedding_model_name)
+        self.embedding_cfg = self.vectordb_cfg["embedding"][self.embedding_backend]
+        self.embedding_model = EMBEDDING[self.embedding_backend](**self.embedding_cfg)
+
+        # Setup kwargs for from_documents
+        self.vectordb_cfg["embedding"] = self.embedding_model
 
         self.debug=debug
-        super().__init__(vectorstore=self.vectorstore, embedding_model=self.embedding_model, debug=self.debug)
+        super().__init__(vectorstore=self.vectorstore, embedding_model=self.embedding_model, vectordb_cfg=self.vectordb_cfg, debug=self.debug)
 
     def chunk_count(self):
         return self.vectorstore._collection.count()
--- tutorials/README.md
+++ tutorials/README.md
@@ -1,3 +1,6 @@
 Online Tutorials:
 Ingestion:
 1. 5 levels of splitting for retrieval: https://www.youtube.com/watch?v=8OJC21T2SL4
+
+Test:
+1. https://github.com/pixegami/rag-tutorial-v2/blob/main/data/ticket_to_ride.pdf
